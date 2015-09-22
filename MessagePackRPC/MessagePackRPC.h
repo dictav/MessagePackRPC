f365a00c381f78867e5deca4f714fdad861c70f5
Shintaro Abe <dictav@gmail.com>
2015-09-22 14:40:55 +0900

    Initial Commit

---
+++ MessagePackRPC/MessagePackRPC.h
@@ -0,0 +1,19 @@
+//
+//  MessagePackRPC.h
+//  MessagePackRPC
+//
+//  Created by 阿部慎太郎 on 9/22/15.
+//  Copyright © 2015 dictav. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MessagePackRPC.
+FOUNDATION_EXPORT double MessagePackRPCVersionNumber;
+
+//! Project version string for MessagePackRPC.
+FOUNDATION_EXPORT const unsigned char MessagePackRPCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessagePackRPC/PublicHeader.h>
+
+
